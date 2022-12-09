475b9b6739bdcc8f5131c4631b05faa16a04dbe5
Arkady Romanov <arkady.romanov@rock-west.com>
2022-12-09 22:53:06 +0200

    Small refactoring

--- questionnaire/contract.go
+++ questionnaire/contract.go
@@ -20,7 +20,7 @@ type (
 	// StartResponse is a response on Start request. It holds Greeting text to
 	// send to a user and a qustion to ask next.
 	StartResponse struct {
-		Greeting Message
+		Welcome  Message
 		Question Question
 	}
 
@@ -33,9 +33,11 @@ type (
 	// Service keeps tracking user's questions and answers. It is responsible to
 	// store user's answers and to return next question to ask
 	Service interface {
-		// Start reset question asked and returns welcome message and the first question
+		// Start reset question asked to the user
+		// and returns welcome message and the first question.
 		Start(ctx context.Context, qnrID uuid.UUID, userID uuid.UUID) (StartResponse, error)
-		// Answer validates, saves and returns next question to ask
+
+		// Answer validates, saves anser and returns next question to ask.
 		Answer(ctx context.Context, qnrID uuid.UUID, userID uuid.UUID, answer Answer) (AnswerResponse, error)
 	}
 )
