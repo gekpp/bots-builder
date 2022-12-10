633a9d10e7465b7a40c74ee71f1c75e8264a6846
Arkady Romanov <arkady.romanov@rock-west.com>
2022-12-10 14:51:49 +0200

    Add schema.sql and run-postrges target to Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
 
+.PHONY: start-postgres
+start-postgres:
+	docker run --rm -d --name pg_bots_builder \
+	-e POSTGRES_PASSWORD=admin \
+	-e POSTGRES_DB=bots_builder \
+	-e POSTGRES_USER=postgres \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v `pwd`/scripts/migrations:/docker-entrypoint-initdb.d/ \
+	-p 5432:5432 \
+	postgres:15.1
+
+
+.PHONY: run
 ifeq ($(MAKECMDGOALS),run)
 include .env/.run.env
 export
@@ -6,6 +19,7 @@ endif
 run:
 	go run cmd/main.go
 
+.PHONY: run-arkady
 ifeq ($(MAKECMDGOALS),run-arkady)
 include .env/.run-arkady.env
 export
---
+++ scripts/migrations/20221210-init-schema.sql
@@ -0,0 +1,79 @@
+create table users (
+    id uuid default gen_random_uuid() not null constraint users_pk primary key,
+    telegram_user_id text,
+    telegram_username text,
+    created_at timestamp without time zone default now() not null,
+    updated_at timestamp without time zone default now() not null
+);
+
+create unique index users_telegram_user_id_uindex on users (telegram_user_id);
+
+create table questionnaires (
+    id uuid default gen_random_uuid() not null constraint questionnaires_pk primary key,
+    name text not null,
+    welcome_message text not null,
+    goodbye_message text not null,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+);
+
+create type question_kind AS ENUM ('open', 'close', 'range');
+
+create table questions (
+    id uuid default gen_random_uuid() not null constraint questions_pk primary key,
+    question text not null,
+    kind question_kind not null,
+    next_question_id uuid,
+    questionnaire_id uuid not null constraint questions_questionnaires_id_fk references questionnaires,
+    rank int not null,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+);
+
+create unique index questions_questionnaire_id_rank_uindex on questions (questionnaire_id, rank);
+
+create table range_answer (
+    id uuid default gen_random_uuid() not null constraint range_answer_pk primary key,
+    question_id uuid not null constraint range_answer_questions_id_fk references questions,
+    minimum int not null,
+    maximum int not null,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+);
+
+create unique index range_answer_question_id_uindex on range_answer (question_id);
+
+create table answer_options (
+    id uuid default gen_random_uuid() not null constraint answer_options_pk primary key,
+    question_id uuid not null constraint answer_options_questions_id_fk references questions,
+    answer text not null,
+    rank integer not null,
+    next_question_id uuid,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+);
+
+create unique index answer_options_question_id_rank_uindex on answer_options (question_id, rank);
+
+create type user_question_state as enum ('asked', 'answered');
+
+create table user_answers (
+    id uuid default gen_random_uuid() not null constraint user_answers_pk primary key,
+    user_id uuid not null,
+    questionnaire_id uuid not null,
+    question_id uuid not null,
+    answer_option_id uuid,
+    range_answer_id uuid,
+    raw_answer text not null,
+    created_at timestamp without time zone default now() not null,
+    question_state user_question_state not null,
+    updated_at timestamp without time zone default now() not null
+);
+
+create index user_answers_user_id_questionnaire_id_created_at_index on user_answers (
+    user_id asc,
+    questionnaire_id asc,
+    created_at desc
+)
+WHERE
+    question_state = 'asked';
